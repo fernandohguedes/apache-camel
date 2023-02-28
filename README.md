fd29a8ffafa9dbcaf67e42a0f021367a0dd5c619
Fernando Guedes <fernando.guedes@cwi.com.br>
2023-02-28 00:44:19 -0300

    feat: Desenvolvendo a primeira rota

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Apache Camel
 * Repositório destinado ao desenvolvimento de estudos sobre rotas com Apache Camel.
+
+# Links Uteis
+* https://camel.apache.org/
+* https://camel.apache.org/components/next/index.html 
---
+++ src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -0,0 +1,32 @@
+package br.com.caelum.camel;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public class RotaPedidos {
+
+    public static void main(String[] args) throws Exception {
+
+        CamelContext context = new DefaultCamelContext();
+        context.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("file:pedidos?delay=5s&noop=true")
+                        // printa o ID gerado pelo Camel durante a execução
+                        .log("${id}")
+                        //converte o conteúdo do arquivo XML em JSON
+                        .marshal().xmljson()
+                        // printa o corpo do JSON
+                        .log("${body}")
+                        // Renomear o arquivo com extensão .json
+                        .setHeader("CamelFileName", simple("${file:name.noext}.json"))
+                        .to("file:saida");
+            }
+        });
+
+        context.start();
+        Thread.sleep(20000);
+        context.stop();
+    }
+}
